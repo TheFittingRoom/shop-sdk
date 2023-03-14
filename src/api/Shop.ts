92c97d2ba4ed1e3735bb3dd75dee4f25bb423960
codeliger <bentiessen@gmail.com>
2023-03-14 15:03:01 -0400

    add size recommender call

--- src/api/Shop.ts
+++ src/api/Shop.ts
@@ -1,9 +1,10 @@
 import { UserProfile } from "firebase/auth";
 import { FriendlyError } from "../auth/errors";
-import { AvatarNotCreated, AvatarNotReady, ErrorResponse} from "../api/errors";
+import { AvatarNotCreated, AvatarNotReady, ErrorResponse } from "../api/errors";
 import { TryOnFrames } from "../types";
-import {FirebaseUser} from "../auth/FirebaseUser";
+import { FirebaseUser } from "../auth/FirebaseUser";
 import { Fetcher } from "./Fetcher";
+import { SizeRecommendation } from "../api/responses";
 
 function IsImgValid(url: string) {
 	const img = new Image();
@@ -16,21 +17,21 @@ function IsImgValid(url: string) {
 }
 
 
-class Shop{
-	private BrandID:string;
+class Shop {
+	private BrandID: string;
 
 	static NoFramesFound = new Error('No frames found for this colorway');
 
-	constructor(brandID: string){
+	constructor(brandID: string) {
 		this.BrandID = brandID;
 	}
 
-	RequestColorwayFrames(colorwayID: number): Promise<void>{
+	RequestColorwayFrames(colorwayID: number): Promise<void> {
 		return new Promise((resolve, reject) => {
 			Fetcher.Post(`/colorways/${colorwayID}/frames`, null).then(() => {
 				resolve();
 			}).catch((error: ErrorResponse) => {
-				switch(error.error) {
+				switch (error.error) {
 					case AvatarNotCreated:
 						reject(new FriendlyError("You have not creatd an avatar", error.error));
 						break
@@ -40,7 +41,8 @@ class Shop{
 					default:
 						reject(new FriendlyError("There was an error when trying on clothing", error.error));
 				}
-			})})
+			})
+		})
 	}
 
 	async GetColorwayFrames(ColorwaySKU: string): Promise<TryOnFrames> {
@@ -53,9 +55,28 @@ class Shop{
 				reject(Shop.NoFramesFound);
 			}).catch((error) => {
 				reject(error);
-			}
-			)
+			})
 		})
 	}
 
+	async GetRecommendedSizes(BrandStyleID: string): Promise<SizeRecommendation | ErrorResponse> {
+		return new Promise((resolve, reject) => {
+			Fetcher.Get(`/styles/${BrandStyleID}/recommendation`, null).then((data) => {
+				resolve(data as SizeRecommendation);
+			}).catch((error: ErrorResponse) => {
+				switch (error.error) {
+					case AvatarNotCreated:
+						reject(new FriendlyError("You have not created an avatar", error.error));
+						break
+					case AvatarNotReady:
+						reject(new FriendlyError("Your avatar is not ready yet", error.error));
+						break
+					default:
+						reject(new FriendlyError("There was an error when trying on clothing", error.error));
+				}
+			})
+
+		})
+	}
 }
+
