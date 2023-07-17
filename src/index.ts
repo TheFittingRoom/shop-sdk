bd11c1d3be383142c521e99e8b15b76387a7c341
Vitaliy Isikov <visikov@gmail.com>
2023-07-17 15:20:16 -0500

    fixies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * as requests from './api/requests'
-export * as response from './api/responses'
+export * as responses from './api/responses'
 export { initShop } from './api/shop'
 export type { TfrShop } from './api/shop'
 export * as Errors from './helpers/errors'
