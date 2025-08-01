0aab6d8c55488f00e9dc65e8993561436b85d673
codeliger <codeliger@protonmail.com>
2025-07-31 20:44:07 -0400

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
+import replace from '@rollup/plugin-replace'
 import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+
 import * as dotenv from 'dotenv'
 
 import pkg from './package.json' with { type: 'json' }
@@ -33,11 +35,35 @@ export default {
       name: 'dotenv',
       buildStart() {
         dotenv.config()
-      }
+      },
     },
+    // Replace process.env variables with their actual values
+    replace({
+      preventAssignment: true,
+      values: {
+        'process.env.DEV_FIREBASE_API_KEY': JSON.stringify(process.env.DEV_FIREBASE_API_KEY),
+        'process.env.DEV_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.DEV_FIREBASE_AUTH_DOMAIN),
+        'process.env.DEV_FIREBASE_PROJECT_ID': JSON.stringify(process.env.DEV_FIREBASE_PROJECT_ID),
+        'process.env.DEV_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.DEV_FIREBASE_STORAGE_BUCKET),
+        'process.env.DEV_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.DEV_FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.DEV_FIREBASE_APP_ID': JSON.stringify(process.env.DEV_FIREBASE_APP_ID),
+        'process.env.DEV_API_ENDPOINT': JSON.stringify(process.env.DEV_API_ENDPOINT),
+        'process.env.DEV_AVATAR_TIMEOUT_MS': JSON.stringify(process.env.DEV_AVATAR_TIMEOUT_MS),
+        'process.env.DEV_VTO_TIMEOUT_MS': JSON.stringify(process.env.DEV_VTO_TIMEOUT_MS),
+        'process.env.PROD_FIREBASE_API_KEY': JSON.stringify(process.env.PROD_FIREBASE_API_KEY),
+        'process.env.PROD_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.PROD_FIREBASE_AUTH_DOMAIN),
+        'process.env.PROD_FIREBASE_PROJECT_ID': JSON.stringify(process.env.PROD_FIREBASE_PROJECT_ID),
+        'process.env.PROD_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.PROD_FIREBASE_STORAGE_BUCKET),
+        'process.env.PROD_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.PROD_FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.PROD_FIREBASE_APP_ID': JSON.stringify(process.env.PROD_FIREBASE_APP_ID),
+        'process.env.PROD_API_ENDPOINT': JSON.stringify(process.env.PROD_API_ENDPOINT),
+        'process.env.PROD_AVATAR_TIMEOUT_MS': JSON.stringify(process.env.PROD_AVATAR_TIMEOUT_MS),
+        'process.env.PROD_VTO_TIMEOUT_MS': JSON.stringify(process.env.PROD_VTO_TIMEOUT_MS),
+      },
+    }),
     nodeResolve(),
     typescript({
-      outputToFilesystem: true
+      outputToFilesystem: true,
     }),
   ],
 }
